148e94e4c15adfbf455d3ff08eb4d64149f27af7
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2021-08-30 17:03:50 -0400

    optimizations

--- Includes/cheats.hpp
+++ Includes/cheats.hpp
@@ -3,12 +3,26 @@
 
 #include <CTRPluginFramework.hpp>
 #include "Unicode.h"
+#include "Helpers/Region.hpp"
+#include "MenuPointers.hpp"
+
 
 namespace CTRPluginFramework
 {
 
-//Folder
+//Stuffs
+	#define TID_JPN 0x000400000015DA00
+	#define TID_USA 0x000400000018BC00
+	#define TID_EUR 0x000400000018F500
+	#define TID_KOR 0x000400000018B500
+	#define Colortop 0x538453FF
+	#define Colorbot 0x33476FF
+	#define keycolor 0xFFBF02FF
+	#define BubbleGum 0xFF126BFF
+	#define TeamDir "teams"
 	using GetNameCallback = void(*)(Keyboard &keyboard, KeyboardEvent &event);
+	void InitMenu(PluginMenu *menu);
+//Folder
 	void SpeedUp(MenuEntry *entry);
 	void TreasureRadar(MenuEntry *entry);
 	void RichCat(MenuEntry *entry);
@@ -23,13 +37,17 @@ namespace CTRPluginFramework
 	void InvincibleCatBase(MenuEntry* entry);
 	void EnemyBaseOneHealth(MenuEntry* entry);
 	void SpecialCoordinateGuy(MenuEntry* entry);
+	bool SetUpKB(std::string msg, bool hex, const int length, u32 &output, u32 def, GetNameCallback cb);
+	bool SetUpKB(std::string msg, bool hex, const int length, u16 &output, u16 def, GetNameCallback cb);
 	bool SetUpKB(std::string msg, bool hex, const int length, u8 &output, u8 def, GetNameCallback cb);
 	void UnitEditor(MenuEntry* entry);
 //other folder
-	void NewCopy(MenuEntry* entry);
+	void NewerCopy(MenuEntry* entry);
+	void SetPaste(MenuEntry* entry);
 	void SaveRandom(MenuEntry* entry);
 	void BackupOptions(MenuEntry* entry);
 	void RestoreOptions(MenuEntry* entry);
+	void unlock(MenuEntry* entry);
 //other other folder
 	void InfiniteXP(MenuEntry* entry);
 	void InfiniteCatFood(MenuEntry* entry);
